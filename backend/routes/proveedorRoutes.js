d2c1d0de8b20c5d8f886a625df053f8ec54889b7
Willy-Leon-Perez <u23246936@utp.edu.pe>
2025-12-10 01:00:53 -0500

    corrección

--- backend/routes/proveedorRoutes.js
+++ backend/routes/proveedorRoutes.js
@@ -1,17 +1,10 @@
 import express from "express";
-<<<<<<< HEAD
 // 🛑 Aquí debes importar el objeto ProveedorController con las funciones en español
 import { ProveedorController } from "../controllers/proveedorController.js"; 
 
 const router = express.Router();
 
 // Rutas CRUD para Proveedores usando el controlador en español
-=======
-import { ProveedorController } from "../controllers/proveedorController.js";
-
-const router = express.Router();
-
->>>>>>> 87921f61849e3e0eb14846645dbdd687c465f7c5
 router.get("/", ProveedorController.listar);
 router.get("/:id", ProveedorController.ver);
 router.post("/", ProveedorController.crear);
